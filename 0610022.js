11b8826c113d866f8087a14c2d90b7127bc87ea9
Justin <justin31452@gmail.com>
2020-06-25 09:40:36 +0800

    boundary detection

--- 0610022.js
+++ 0610022.js
@@ -3,43 +3,100 @@ const rowEnemies = 2;
 const enemy = {
     width: 50,
     height: 50,
+    damage: 1,
+    speed: 10
 }
 const rocket = {
-    width: 10,
-    height: 16
+        width: 10,
+        height: 16,
+        damage: 1,
+        speed: 5
+    }
+    //status
+const MENU = 0;
+const GAME = 1;
+const LOSE = 2;
+
+var ship = {
+    width: 50,
+    height: 50,
+    top: 700,
+    left: 600,
+    speed: 10,
+    lives: 3
 }
 var rockets = [];
 var enemies = [];
-var ship = {
-    top: 700,
-    left: 600,
-    speed: 10
-}
-
-
+var status = MENU;
 
 document.onkeydown = function(e) {
     //<-
     if (e.keyCode == 37) {
-        ship.left -= ship.speed;
-        moveship();
+        if (ship.left > ship.speed) {
+            ship.left -= ship.speed;
+            console.log(ship.left);
+            drawship();
+        }
     }
     //->
     else if (e.keyCode == 39) {
-        ship.left += ship.speed;
-        moveship();
+        if (ship.left + ship.width < 1200 + ship.speed) {
+            ship.left += ship.speed;
+            console.log(ship.left);
+            drawship();
+        }
     }
     //space
     else if (e.keyCode == 32) {
         rockets.push({
             left: ship.left + 25,
-            top: ship.top + 10
+            top: ship.top + 10,
+            lives: 1
         });
         rockets.push({
             left: ship.left,
-            top: ship.top + 10
+            top: ship.top + 10,
+            lives: 1
         });
         drawRocket();
+    }
+}
+
+function DeathDetection() {
+    for (var e = 0; e < enemies.length; e++) {
+        if (enemies[e].lives <= 0) {
+            enemies.splice(e, 1);
+        }
+    }
+    for (var r = 0; r < rockets.length; r++) {
+        if (rockets[r].lives <= 0) {
+            rockets.splice(r, 1);
+        }
+    }
+    if (ship.lives <= 0) {
+        status = LOSE;
+    }
+}
+
+function shipEnemyCollision() {
+    for (var e = 0; e < enemies.length; e++) {
+        if ((ship.top <= enemies[e].top + enemy.height) &&
+            (ship.top + ship.height >= enemies[e].top) &&
+            (ship.left + ship.width >= enemies[e].left) &&
+            (ship.left <= enemies[e].left + enemy.width)
+        ) {
+            ship.lives -= enemy.damage;
+            enemies[e].lives = -1;
+        }
+
+    }
+}
+
+function enemyWallCollision() {
+    for (var e = 0; e < enemies.length; e++) {
+        if (enemies[e].top + enemy.height > 800) {
+            enemies[e].lives = -1;
+        }
     }
 }
 
@@ -51,8 +108,8 @@ function enemyRocketCollision() {
                 (rockets[r].left + rocket.width >= enemies[e].left) &&
                 (rockets[r].left <= enemies[e].left + enemy.width)
             ) {
-                rockets.splice(r, 1);
-                enemies.splice(e, 1);
+                rockets[r].lives--;
+                enemies[e].lives -= rocket.damage;
             }
         }
     }
@@ -64,7 +121,8 @@ function createEnemy() {
     for (var i = 0; i < numEnemies; i++) {
         enemies.push({
             left: lft,
-            top: tp
+            top: tp,
+            lives: 1
         });
         lft += 100;
         if ((i + 1) % (numEnemies / rowEnemies) == 0) {
@@ -97,15 +155,15 @@ function drawRocket() {
 
 function moveEnemy() {
     for (var i = 0; i < enemies.length; i++)
-        enemies[i].top += 3;
+        enemies[i].top += enemy.speed;
 }
 
 function moveRocket() {
     for (var i = 0; i < rockets.length; i++)
-        rockets[i].top -= 5;
+        rockets[i].top -= rocket.speed;
 }
 
-function moveship() {
+function drawship() {
     document.getElementById("ship").style.left = ship.left + "px";
 }
 
@@ -118,5 +176,8 @@ function loop() {
     drawEnemy();
     moveEnemy();
     enemyRocketCollision();
+    shipEnemyCollision();
+    enemyWallCollision();
+    DeathDetection();
     setTimeout(loop, 100);
 }
